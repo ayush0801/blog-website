ca1fec9cdaf8ae9cb2dde098f15c908a88acb3e7
Ayush <jha.4@iitj.ac.in>
2024-02-16 01:38:05 +0530

    created UPDATE and DELETE APIs

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -2,7 +2,7 @@ const blogModel = require('../models/BlogModel');
 const isEmailValid = require('../utils/validation');
 require('../db');
 
-
+//GET all Posts
 module.exports.all_posts_get = async(req, res) => {
    try{
       const blogs = await(blogModel.find()); 
@@ -13,6 +13,7 @@ module.exports.all_posts_get = async(req, res) => {
    }
 }
 
+//GET a particular Post
 module.exports.get_particular_blog_get =  (req,res) => {
    const blogID = req.params.id;
    blogModel.findOne({_id: blogID})
@@ -26,6 +27,7 @@ module.exports.get_particular_blog_get =  (req,res) => {
    })
 }
 
+//Create a Post
 module.exports.add_blog_post = async(req, res) => {
    const {title, body, tags} = req.body
    if(!title || !body){
@@ -39,4 +41,69 @@ module.exports.add_blog_post = async(req, res) => {
    catch(err){
       res.status(500).send({error: err.message});
    }
+}
+
+//Update a Post
+module.exports.update_post = async(req,res) => {
+   const blogId = req.params.id;
+   const thingsToUpdate = {}
+   const {title, body, tags} = req.body
+   if(title && body){
+      if(title.length < 3){
+          return res.status(422).json({error: "Title should be atleast 3 characters"})
+      }
+      if(body.length < 10){
+          return res.status(422).json({error: "Body should be atleast 10 characters"})
+      }
+      thingsToUpdate.title = title
+      thingsToUpdate.body = body
+   }
+   else if(title){
+      if(title.length < 3){
+          return res.status(422).json({error: "Title should be atleast 3 characters"})
+      }
+      thingsToUpdate.title = title
+   }
+   else if(body){
+      if(body.length < 10){
+          return res.status(422).json({error: "Body should be atleast 10 characters"})
+      }
+      thingsToUpdate.body = body
+   }
+   else{
+      return res.status(422).json({error: "Please fill any one field"})
+   }
+   
+   blogModel.findOneAndUpdate({_id: blogId}, {$set: thingsToUpdate})
+   .then((data) => {
+      if(!data){
+         res.json('Data not found');
+      }
+      else{
+         res.status(200).json('Data updated successfully');
+      }
+   })
+   .catch((err) => {
+      res.status(400).json({error: err})
+   })
+
+}
+
+//Delete a Post
+module.exports.delete_post = async(req, res) => {
+   const blogId = req.params.id;
+   blogModel.findOneAndDelete({_id: blogId})
+   .then((data) => {
+      if(!data){
+         console.log(err);
+         res.json({error: 'No blog Found'});
+      }
+      else{
+         res.status(200).json({message: 'Post Delete Successfully'});
+      }
+   })
+   .catch((err) => {
+      console.log(err);
+      res.status(400).json({error: err});
+   })
 }
--- router/blogRoute.js
+++ router/blogRoute.js
@@ -7,6 +7,7 @@ const router = Router();
 router.get('/posts', blogController.all_posts_get);
 router.post('/posts', blogController.add_blog_post);
 router.get('/posts/:id', blogController.get_particular_blog_get);
-   
+router.delete('/posts/:id', blogController.delete_post);
+router.put('/posts/:id', blogController.update_post)
 
 module.exports = router;   
